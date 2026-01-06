28901a937512c75f96f29dbb5e120e67febd503c
HyounJun <123528558+HyounJun@users.noreply.github.com>
2026-01-06 21:19:52 +0900

    Update ecommerce_events_analysls.sql

--- ecommerce_events_analysls.sql
+++ ecommerce_events_analysls.sql
@@ -1,4 +1,9 @@
+--데이터확인
+SELECT *
+from 2019_dec
+limit 10
 
+-- 상품 페이지 - 장바구니 - 구매 퍼널별 전환율
 WITH view_person as (  --상품을 본 사람
         SELECT event_time
         ,user_id
@@ -28,3 +33,36 @@ from view_person
     left JOIN purchase_person on purchase_person.user_id = cart_person.user_id
                              AND purchase_person.purchase_at = cart_person.cart_at
                              AND purchase_person.event_time >= cart_person.event_time -- 장바구니에 담은 세션과 구매한 세션이 같은것도 포함
+
+-- 상품 페이지 - 장바구니 - 삭제 전환율
+WITH view_person as ( --제품을 본 사람  
+        SELECT event_time
+        ,user_id
+        ,user_session as view_at
+        from 2019_dec
+        where event_type = 'view'
+),cart_person as ( -- 장바구니에 담은 사람
+        SELECT event_time
+        ,user_id
+        ,user_session as cart_at
+        from 2019_dec
+        where event_type = 'cart'
+),remove_person as ( -- 장바구니에서 삭제한사람
+        SELECT event_time
+        ,user_id
+        ,user_session as remove_at
+        from 2019_dec
+        where event_type = 'remove_from_cart'
+)
+SELECT count(DISTINCT view_person.user_id , view_person.view_at) as view_cnt
+       ,count(DISTINCT cart_person.user_id , cart_person.cart_at) as cart_after
+       ,count(DISTINCT remove_person.user_id , remove_person.remove_at) as remove_at_after
+from view_person
+    left join cart_person on view_person.user_id = cart_person.user_id
+                         AND view_person.view_at = cart_person.cart_at
+                         AND view_person.event_time <= cart_person.event_time -- 제품을 본 세션과 장바구니에 담은세션이 같은것도 포함
+    left JOIN remove_person on remove_person.user_id = cart_person.user_id
+                             AND remove_person.remove_at = cart_person.cart_at
+                             AND remove_person.event_time >= cart_person.event_time -- 장바구니에 담은 뒤 삭제
+
+
