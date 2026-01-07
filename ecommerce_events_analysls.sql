3b53ad3c5fc62727b2e1cdb77e6a127174ae9942
HyounJun <123528558+HyounJun@users.noreply.github.com>
2026-01-07 15:56:27 +0900

    Update ecommerce_events_analysls.sql

--- ecommerce_events_analysls.sql
+++ ecommerce_events_analysls.sql
@@ -2,8 +2,32 @@
 SELECT *
 from 2019_dec
 limit 10
+      
+--EDA
+--category_code별 값 확인 (공백은 Unknow처리)
+SELECT category
+       ,count(*) as cnt
+FROM(
+        SELECT *
+               ,case WHEN category_code ='' then 'Unknow' else category_code end as category
+        from 2019_dec
+)as category_unknow
+group by category
+order by cnt desc
 
--- 상품 페이지 - 장바구니 - 구매 퍼널별 전환율
+--category_code별 값 확인 (공백은 Unknow처리)
+SELECT brand_srot
+       ,count(*) as cnt
+FROM(
+        SELECT *
+               ,case WHEN brand ='' then 'Unknow' else brand end as brand_srot
+        from 2019_dec
+)as brand_unknow
+group by brand_srot
+order by cnt desc
+
+
+-- 상품 페이지 - 장바구니 - 구매 퍼널별 전환 수
 WITH view_person as (  --상품을 본 사람
         SELECT event_time
         ,user_id
@@ -33,36 +57,4 @@ from view_person
     left JOIN purchase_person on purchase_person.user_id = cart_person.user_id
                              AND purchase_person.purchase_at = cart_person.cart_at
                              AND purchase_person.event_time >= cart_person.event_time -- 장바구니에 담은 세션과 구매한 세션이 같은것도 포함
-
--- 상품 페이지 - 장바구니 - 삭제 전환율
-WITH view_person as ( --제품을 본 사람  
-        SELECT event_time
-        ,user_id
-        ,user_session as view_at
-        from 2019_dec
-        where event_type = 'view'
-),cart_person as ( -- 장바구니에 담은 사람
-        SELECT event_time
-        ,user_id
-        ,user_session as cart_at
-        from 2019_dec
-        where event_type = 'cart'
-),remove_person as ( -- 장바구니에서 삭제한사람
-        SELECT event_time
-        ,user_id
-        ,user_session as remove_at
-        from 2019_dec
-        where event_type = 'remove_from_cart'
-)
-SELECT count(DISTINCT view_person.user_id , view_person.view_at) as view_cnt
-       ,count(DISTINCT cart_person.user_id , cart_person.cart_at) as cart_after
-       ,count(DISTINCT remove_person.user_id , remove_person.remove_at) as remove_at_after
-from view_person
-    left join cart_person on view_person.user_id = cart_person.user_id
-                         AND view_person.view_at = cart_person.cart_at
-                         AND view_person.event_time <= cart_person.event_time -- 제품을 본 세션과 장바구니에 담은세션이 같은것도 포함
-    left JOIN remove_person on remove_person.user_id = cart_person.user_id
-                             AND remove_person.remove_at = cart_person.cart_at
-                             AND remove_person.event_time >= cart_person.event_time -- 장바구니에 담은 뒤 삭제
-
 
