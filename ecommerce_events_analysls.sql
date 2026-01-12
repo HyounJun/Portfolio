9fb48eb899ef172c8d1d3069d30fbcace841dfca
HyounJun <123528558+HyounJun@users.noreply.github.com>
2026-01-12 18:34:05 +0900

    Update ecommerce_events_analysls.sql

--- ecommerce_events_analysls.sql
+++ ecommerce_events_analysls.sql
@@ -4,6 +4,16 @@ from 2019_dec
 limit 10
       
 --EDA
+      
+-- 일별 방문자(DAU)
+-- 일별 방문자수를 구해본 결과 특정 날짜에 몰리는 이상치가 발견되지 않아 변동성이 낮았습니다. 
+SELECT 
+    DATE(event_time) AS event_date,
+    COUNT(DISTINCT user_id) AS DAU 
+FROM 2019_dec
+GROUP BY event_date
+ORDER BY event_date
+      
 --category_code별 값 확인 (공백은 Unknow처리)
 SELECT category
        ,count(*) as cnt
@@ -17,23 +27,21 @@ order by cnt desc
 
 --brand 값 확인 (공백은 Unknow처리)
 SELECT brand_srot
-       ,count(*) as cnt
+       ,cnt
+       ,ROUND(cnt * 100.0 / SUM(cnt) OVER(), 2) as pct
 FROM(
-        SELECT *
-               ,case WHEN brand ='' then 'Unknown' else brand end as brand_srot
+        SELECT case WHEN brand ='' then 'Unknown' else brand end as brand_srot
+                ,count(*) as cnt
         from 2019_dec
+        GROUP BY 1
 )as brand_unknow
-group by brand_srot
 order by cnt desc
 
---일별 방문자
-SELECT 
-    DATE(event_time) AS event_date,
-    COUNT(DISTINCT user_id) AS DAU 
-FROM 2019_dec
-GROUP BY event_date
-ORDER BY event_date
-      
+/*
+brand별로 구분해본 결과 Unknown의 비중이 42.74%로 가장 높았으며, 브랜드가 있는 데이터중에서는 runail이 7.25%로 가장 높게 나타났습니다.
+category별로 구분해본 결과 Unknown의 비중이 98.35%로 카테고리별 데이터가 많이 누락됐음을 알 수 있습니다.
+*/ 
+
 --funnel
 -- 상품 페이지 - 장바구니 - 구매 퍼널별 전환 비율
 WITH view_person as (  --상품을 본 사람
